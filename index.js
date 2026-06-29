f6c1ed8760a4bf87b1658ae3d7100339a2323833
maoya12345 <jannatulmaoya456@gmail.com>
2026-06-29 23:33:56 +0600

    updated

--- index.js
+++ index.js
@@ -284,4 +284,11 @@ async function server() {
 server().catch(console.dir);
 
 app.get('/', (req, res) => res.send('Server is running...'));
-app.listen(port, () => console.log(`Server running on port ${port}`));
+//app.listen(port, () => console.log(`Server running on port ${port}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+  });
+}
+
+export default app;
